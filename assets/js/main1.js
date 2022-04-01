845396101e845e43e660f6e09a2aed2f723d2500
78741722Ale <pecorillaalessandro@gmail.com>
2022-04-01 14:16:32 +0200

    Initial Commit

---
+++ assets/js/main1.js
@@ -0,0 +1,7 @@
+// Consegna Palindromi
+
+/* 
+Chiedere all’utente di inserire una parola Creare una 
+funzione per capire se la parola inserita è palindroma 
+*/
+
---
+++ assets/js/main2.js
@@ -0,0 +1,1 @@
+// Consegna pari o dispari
